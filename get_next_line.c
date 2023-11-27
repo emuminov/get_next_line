29fbd23b27eecfc61810d64ab8a3094863cc039e
Eldar Muminov <emuminov@z2r4p2.42lyon.fr>
2023-11-27 15:12:05 +0100

    style: normalized most of the errors

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 22:46:03 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/27 15:06:27 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/27 15:11:43 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,53 +45,53 @@ char	*linked_list_str_join(t_linked_list *list)
 	return (res);
 }
 
-void	create_leftovers(char *buff, ssize_t index, ssize_t size, t_file *file)
+void	create_leftovers(char *buff, ssize_t index, ssize_t sz, t_file *f)
 {
 	char	*res;
 	ssize_t	i;
 
-	res = malloc(sizeof(char) * (size - index));
+	res = malloc(sizeof(char) * (sz - index));
 	if (!res)
 	{
-		if (file->leftovers)
-			free(file->leftovers);
-		file->leftovers = NULL;
+		if (f->leftovers)
+			free(f->leftovers);
+		f->leftovers = NULL;
 		return ;
 	}
 	i = 0;
-	while ((i + 1) < (size - index))
+	while ((i + 1) < (sz - index))
 	{
 		res[i] = buff[index + i + 1];
 		i++;
 	}
 	res[i] = '\0';
-	if (file->leftovers)
-		free(file->leftovers);
-	file->leftovers = res;
-	file->leftovers_len = i;
+	if (f->leftovers)
+		free(f->leftovers);
+	f->leftovers = res;
+	f->leftovers_len = i;
 }
 
-char	*create_node_content(ssize_t size, char *buff, t_file *file, t_node *node)
+char	*create_node_content(ssize_t sz, char *buff, t_file *f, t_node *node)
 {
 	char	*res;
 	ssize_t	i;
 
-	res = malloc(sizeof(char) * (size + 1));
+	res = malloc(sizeof(char) * (sz + 1));
 	if (!res)
 		return (0);
 	i = 0;
-	while (buff[i] && buff[i] != '\n' && i < size)
+	while (buff[i] && buff[i] != '\n' && i < sz)
 	{
 		res[i] = buff[i];
 		i++;
 	}
 	if (buff[i] == '\0')
-		file->file_ended = 1;
+		f->file_ended = 1;
 	else if (buff[i] == '\n')
 	{
-		create_leftovers(buff, i, size, file);
+		create_leftovers(buff, i, sz, f);
 		res[i++] = '\n';
-		file->line++;
+		f->line++;
 	}
 	res[i] = '\0';
 	node->content = res;
@@ -116,7 +116,7 @@ void	free_linked_list(t_linked_list *list)
 	free(list);
 }
 
-t_node	*create_new_node(ssize_t size, t_file *file, char *buff, t_linked_list *list)
+t_node	*create_new_node(ssize_t sz, t_file *f, char *buff, t_linked_list *list)
 {
 	t_node	*node;
 
@@ -127,7 +127,7 @@ t_node	*create_new_node(ssize_t size, t_file *file, char *buff, t_linked_list *l
 		free(buff);
 		return (0);
 	}
-	if (!create_node_content(size, buff, file, node))
+	if (!create_node_content(sz, buff, f, node))
 	{
 		free_linked_list(list);
 		return (0);
@@ -144,25 +144,27 @@ t_node	*create_new_node(ssize_t size, t_file *file, char *buff, t_linked_list *l
 	return (node);
 }
 
-t_linked_list	*init_linked_list(t_file *file)
+t_linked_list	*init_linked_list(t_file *f)
 {
 	t_linked_list	*list;
 	t_node			*node;
-	list = malloc(sizeof(t_linked_list)); if (!list)
+
+	list = malloc(sizeof(t_linked_list));
+	if (!list)
 	{
-		if (file->leftovers)
-			free(file->leftovers);
+		if (f->leftovers)
+			free(f->leftovers);
 		return (0);
 	}
 	list->head = NULL;
 	list->tail = NULL;
-	if (file->leftovers)
+	if (f->leftovers)
 	{
-		node = create_new_node(file->leftovers_len, file, file->leftovers, list);
+		node = create_new_node(f->leftovers_len, f, f->leftovers, list);
 		if (!node)
 		{
-			if (file->leftovers)
-				free(file->leftovers);
+			if (f->leftovers)
+				free(f->leftovers);
 			free(list);
 			return (0);
 		}
@@ -170,20 +172,20 @@ t_linked_list	*init_linked_list(t_file *file)
 	return (list);
 }
 
-t_linked_list	*read_line(int fd, int current_line, t_file *file, char buff[BUFFER_SIZE])
+t_linked_list	*read_line(int fd, int cl, t_file *f, char buff[BUFFER_SIZE])
 {
-	ssize_t 		size;
-	t_linked_list 	*list;
+	ssize_t			sz;
+	t_linked_list	*list;
 
-	list = init_linked_list(file);
+	list = init_linked_list(f);
 	if (!list)
 		return (0);
-	while (current_line == file->line)
+	while (cl == f->line)
 	{
-		size = read(fd, buff, BUFFER_SIZE);
-		if (size <= 0)
+		sz = read(fd, buff, BUFFER_SIZE);
+		if (sz <= 0)
 			return (0);
-		if (!create_new_node(size, file, buff, list))
+		if (!create_new_node(sz, f, buff, list))
 		{
 			free_linked_list(list);
 			return (0);
@@ -194,27 +196,27 @@ t_linked_list	*read_line(int fd, int current_line, t_file *file, char buff[BUFFE
 
 char	*get_next_line(int fd)
 {
-	static t_file	file;
+	static t_file	f;
 	t_linked_list	*list;
 	char			*res;
 	char			buff[BUFFER_SIZE];
 
 	if (BUFFER_SIZE <= 0 || fd == -1)
 	{
-		if (file.leftovers)
-			free(file.leftovers);
+		if (f.leftovers)
+			free(f.leftovers);
 		return (0);
 	}
-	list = read_line(fd, file.line, &file, buff);
+	list = read_line(fd, f.line, &f, buff);
 	if (!list)
 	{
-		if (file.leftovers)
-			free(file.leftovers);
+		if (f.leftovers)
+			free(f.leftovers);
 		return (0);
 	}
 	res = linked_list_str_join(list);
-	if (!res && file.leftovers)
-		free(file.leftovers);
+	if (!res && f.leftovers)
+		free(f.leftovers);
 	free_linked_list(list);
 	return (res);
 }
